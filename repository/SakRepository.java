27aaae81043f3314d598471e0a84b8b6765f5420
Harry Koder <143705948+henrikmansson13235057@users.noreply.github.com>
2025-10-28 09:29:02 +0100

    Remove old duplicate files from root - using proper src structure

--- repository/SakRepository.java
+++
@@ -1,7 +0,0 @@
-package com.fulkoping.uthyrning.repository;
-
-import com.fulkoping.uthyrning.model.Sak;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SakRepository extends JpaRepository<Sak, Long> {
-}
